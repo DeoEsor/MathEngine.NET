0f38fe7d17e5bd0ea81f51459eb15c38f697062d
DeoEsor <RulianValeriy@yandex.ru>
2022-09-17 23:29:05 +0300

    added unary node for expression tree

---
+++ MathEngine.NET/Core/ExpressionTree/NodeUnary.cs
@@ -0,0 +1,24 @@
+﻿using MathEngine.NET.Interfaces;
+
+namespace MathEngine.NET.Core.ExpressionTree;
+
+internal sealed class NodeUnary : Node
+{
+    private readonly Func<double, double> _op;
+
+    private readonly Node _rhs;
+
+    public NodeUnary(Node rhs, Func<double, double> op)
+    {
+        _rhs = rhs;
+        _op = op;
+    }
+
+    public override double Eval(IContext ctx)
+    {
+        var rhsVal = _rhs.Eval(ctx);
+
+        var result = _op(rhsVal);
+        return result;
+    }
+}
