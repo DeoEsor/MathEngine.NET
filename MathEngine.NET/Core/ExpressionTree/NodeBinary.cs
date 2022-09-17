d90bd6944b463324eac9f6af912fea20debafce0
DeoEsor <RulianValeriy@yandex.ru>
2022-09-17 23:28:26 +0300

    added binary node for expression tree

---
+++ MathEngine.NET/Core/ExpressionTree/NodeBinary.cs
@@ -0,0 +1,26 @@
+﻿using MathEngine.NET.Interfaces;
+
+namespace MathEngine.NET.Core.ExpressionTree;
+
+internal class NodeBinary : Node
+{
+    private readonly Node _lhs;
+    private readonly Func<double, double, double>? _op;
+    private readonly Node _rhs;
+
+    public NodeBinary(Node lhs, Node rhs, Func<double, double, double>? op)
+    {
+        _lhs = lhs;
+        _rhs = rhs;
+        _op = op;
+    }
+
+    public override double Eval(IContext ctx)
+    {
+        var lhsVal = _lhs.Eval(ctx);
+        var rhsVal = _rhs.Eval(ctx);
+
+        var result = _op(lhsVal, rhsVal);
+        return result;
+    }
+}
