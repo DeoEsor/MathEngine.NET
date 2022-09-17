ba2234b109ddc974573102b6e80e863acf18ccc8
DeoEsor <RulianValeriy@yandex.ru>
2022-09-17 23:28:36 +0300

    added func node for expression tree

---
+++ MathEngine.NET/Core/ExpressionTree/NodeFunctionCall.cs
@@ -0,0 +1,25 @@
+﻿using MathEngine.NET.Interfaces;
+
+namespace MathEngine.NET.Core.ExpressionTree;
+
+public sealed class NodeFunctionCall : Node
+{
+    private readonly Node[] _arguments;
+
+    private readonly string _functionName;
+
+    public NodeFunctionCall(string functionName, Node[] arguments)
+    {
+        _functionName = functionName;
+        _arguments = arguments;
+    }
+
+    public override double Eval(IContext ctx)
+    {
+        var argValues = new double[_arguments.Length];
+        for (var i = 0; i < _arguments.Length; i++)
+            argValues[i] = _arguments[i].Eval(ctx);
+
+        return ctx.CallFunction(_functionName, argValues);
+    }
+}
