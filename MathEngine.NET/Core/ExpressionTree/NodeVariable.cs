bc52530c5c7ad8c68119b75f9c660382b3eec28d
DeoEsor <RulianValeriy@yandex.ru>
2022-09-17 23:28:54 +0300

    added var node for expression tree

---
+++ MathEngine.NET/Core/ExpressionTree/NodeVariable.cs
@@ -0,0 +1,18 @@
+﻿using MathEngine.NET.Interfaces;
+
+namespace MathEngine.NET.Core.ExpressionTree;
+
+public class NodeVariable : Node
+{
+    private readonly string _variableName;
+
+    public NodeVariable(string variableName)
+    {
+        _variableName = variableName;
+    }
+
+    public override double Eval(IContext ctx)
+    {
+        return ctx.ResolveVariable(_variableName);
+    }
+}
