8d0dbac741b03a5cec5ebf433601166ab7a330a9
DeoEsor <RulianValeriy@yandex.ru>
2022-09-17 23:28:46 +0300

    added num node for expression tree

---
+++ MathEngine.NET/Core/ExpressionTree/NodeNumber.cs
@@ -0,0 +1,18 @@
+﻿using MathEngine.NET.Interfaces;
+
+namespace MathEngine.NET.Core.ExpressionTree;
+
+internal sealed class NodeNumber : Node
+{
+    private readonly double _number;
+
+    public NodeNumber(double number)
+    {
+        _number = number;
+    }
+
+    public override double Eval(IContext ctx)
+    {
+        return _number;
+    }
+}
